86967f4253adb61eba3ee9098066133eee692b73
Alex Bernadí <abernadi@konodrac.com>
2022-03-24 19:49:36 +0100

    Modelo armado con pasos espaciales, falta acabar

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 %%
 
 %%%%%%%% DATOS CASQUETE %%%%%%%
-d = 0.01;  %Distancia mínima entre nodos
+d = c/40;  %Distancia mínima entre nodos
 r = 1;     %Radio casquete
 h = 0.3;   %Altura casquete
 
@@ -13,14 +13,33 @@ zmin = 2;
 n = 1000;
 
 %%%%%%% DATOS REFLECTANCIA %%%%%
-k = 1;
-Ew = 1;
+rof = 998e-9; %*e-9
+c = 1.5;
+rop = 1064e-9;
+cpl = 1.585;
+cps = 0;
+a = 5e-3;
 %%
-
 Nodes = generate_mesh(r,h,d);
 points = generate_random_points(x, y, z, zmin, n);
-T = mat_T(Nodes, points, k);
 
+pos = 0:0.16:3;
+frecs = linspace(0,150,4096);
+
+SL2 = mod_reflectores_Alex(rof,c,rop,cpl,cps,a,frecs);
+E = fft(sen);
+sum
+for j=1:length(pos)
+    for i=1:length(frecs)
+        E_ = E(i)*ones(length(Nodes),1);
+        k = 2*pi/(frecs(i)*c);
+        T = mat_T(Nodes, points, k);
+        a = SL2(i)*eye(n);
+        R = a*T;
+        F = T'*R*T*E_;
+        sum_F(i,j) = sum(F);
+    end
+end
 
 scatter3(points(:,1), points(:,2), points(:,3), "filled")
 hold on
