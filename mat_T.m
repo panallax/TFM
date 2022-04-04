2c21887c2ba0cf626c1cfb1614dc946ea79502ca
Alex Bernadí <abernadi@konodrac.com>
2022-04-04 02:15:31 +0200

    Improvements in main

--- mat_T.m
+++ mat_T.m
@@ -1,10 +1,10 @@
 function [T] = mat_T(nodes, points, k)
 
     T = zeros(length(points), length(nodes));
-    for m=1:length(points)
-        for n=1:legth(nodes)
+    for m=1:size(points,1)
+        for n=1:length(nodes)
             dist = norm(points(m,:) - nodes(n,:));
-            T(m,n) = exp^(-1i*k*dist)/dist; 
+            T(m,n) = exp(-1i*k*dist)/dist; 
         end
     end
 
