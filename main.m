daee38751654f0cc54dded9eeb73b05bd4cd5b18
Alex Bernadí <abernadi@konodrac.com>
2022-04-07 09:50:00 +0200

    Add plot image

--- main.m
+++ main.m
@@ -22,6 +22,7 @@ h = 0.3;   %Altura casquete
 tic
 Nodes = generate_mesh(r,h,d);
 points = generate_random_points(x, y, z, zmin, n);
+points = [1.5 0 1];
 sen = load('sen_Alex.mat');
 pos = 0:0.16:3;
 frecs = linspace(0.01,150,4096);
@@ -31,13 +32,12 @@ E = fft(sen.sen);
 sum_F = zeros(length(frecs),length(pos));
 
 for j=1:length(pos)
-    Nodes(:,1) = Nodes(:,1) + pos(j);
+    Nodes(:,1) = Nodes(:,1) + 0.16;
     for i=1:length(frecs)
         E_ = E(i)*ones(length(Nodes),1);
         k = 2*pi/(frecs(i)*c);
         T = mat_T(Nodes, points, k);
-        a = SL2(i)*eye(n);
-        R = a;
+        R = SL2(i)*eye(n);
         F = T.'*R*T*E_;
         sum_F(i,j) = sum(F);
     end
@@ -48,8 +48,21 @@ for i=1:length(pos)
     sum_F_t(:,i) = ifft(sum_F(:,i));
 end
 
-
+imagesc(abs(sum_F_t))
 toc
 scatter3(points(:,1), points(:,2), points(:,3), "filled")
 hold on
 scatter3(Nodes(:,1),Nodes(:,2),Nodes(:,3), "filled")
+
+
+
+% nImages = length(pos);
+% 
+% fig = figure;
+% for idx = 1:nImages
+%     Nodes(:,1) = Nodes(:,1) + 0.16;
+%     scatter3(Nodes(:,1),Nodes(:,2),Nodes(:,3), "filled")
+%     drawnow
+%     frame = getframe(fig);
+%     im{idx} = frame2im(frame);
+% end
