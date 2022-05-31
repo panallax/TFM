1118f08afe86b7f152466ba957b05579f4388843
Alex Bernadí <abernadi@konodrac.com>
2022-05-31 18:24:13 +0200

    Mejora tiempos de ejecución

--- mat_T.m
+++ mat_T.m
@@ -2,8 +2,9 @@ function [T] = mat_T(nodes, points, k)
 
     T = zeros(length(points), length(nodes));
     for m=1:size(points,1)
-        for n=1:length(nodes)
-            dist = norm(points(m,:) - nodes(n,:));
+        point = points(m,:);
+        parfor n=1:length(nodes)
+            dist = norm(point - nodes(n,:));
             T(m,n) = exp(-1i*k*dist)/dist; 
         end
     end
